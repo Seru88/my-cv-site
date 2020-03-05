0e0cc171eb728532922682faa90daf5c3931afee
Sergio Nizama <sergio.nizama@outlook.com>
2020-03-04 23:40:01 -0500

    more touching

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ class Home extends React.Component {
               <Col xs={4} className='avatar'>
                 <img
                   className='avatar__image'
-                  src='/images/avatar.jpeg'
+                  src={siteConfig.siteCover}
                   alt='user avatar'
                 />
                 <div className="social">
